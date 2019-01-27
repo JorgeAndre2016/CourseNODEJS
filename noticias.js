158bfec5043229521cc1e9e73a0e90ab5be9507b
Jorge <jorge_aquino@hotmail.com.br>
2019-01-27 01:03:46 -0200

    Inserindo categorias

--- noticias.js
+++ noticias.js
@@ -1,7 +1,21 @@
 var http = require('http'); // import library http
 
-var server = http.createServer( function(req, res) {
-    res.end("<html><body>Resposta server new</body></html>");
+var server = http.createServer(function (req, res) {
+    var categoria = req.url;
+    
+    if (categoria == "/tecnologia") {
+        res.end("<html><body>Portal de tecnologia</body></html>");
+    }
+    else if (categoria == "/moda") {
+        res.end("<html><body>Portal de moda</body></html>");
+    }
+    else if (categoria == "/beleza") {
+        res.end("<html><body>Portal de beleza</body></html>");
+    } 
+    else {
+        res.end("<html><body>Portal de Noticias</body></html>");
+    }
+
 });
 
 server.listen(3000); // escutar/verificar o que esta sendo requisitado na porta
