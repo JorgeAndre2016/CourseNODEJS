0101125a6bd5da56b9452e43be30f30999380906
Jorge <jorge_aquino@hotmail.com.br>
2019-01-27 23:43:04 -0200

    Definindo rotas

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ var app = express(); // excutando função que modulo retorna
 app.set('view engine', 'ejs'); // engine de view do express agora é o EJS
 
 app.get('/', function(req, res) {
-    res.send('<html><body>Portal de Noticias</body></html>')
+    res.render('home/index')
 });
 
-app.get('/tecnologia', function(req, res) {
-    res.send('<html><body>Noticias de tecnologia</body></html>')
+app.get('/formulario_inclusao_noticia', function(req, res) {
+    res.render('admin/form_add_noticia')
+});
+
+app.get('/noticias', function(req, res) {
+    res.render('noticias/noticias')
 });
 
 app.listen(3000, function() {
