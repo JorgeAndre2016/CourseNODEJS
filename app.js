f394aefcdcf26954b81a27012414f5ee1b071e3a
Jorge <jorge_aquino@hotmail.com.br>
2019-01-28 23:47:07 -0200

    First module

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var express = require('express'); // import library express
+var msg = require('./mod_teste');
+
 var app = express(); // excutando função que modulo retorna
 
 app.set('view engine', 'ejs'); // engine de view do express agora é o EJS
@@ -17,5 +19,5 @@ app.get('/noticias', function(req, res) {
 
 app.listen(3000, function() {
     console.log("Servidor rodando com Express");
-    
+    console.log(msg());
 });
