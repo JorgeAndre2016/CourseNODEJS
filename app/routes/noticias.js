376ca86ef4ecff85e860f5ffee9287f7c8e721c2
Jorge <jorge_aquino@hotmail.com.br>
2019-01-31 00:13:18 -0200

    acessando banco

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,22 @@
 module.exports = function (app) {
+
     app.get('/noticias', function (req, res) {
-        res.render('noticias/noticias')
+
+        var mysql = require('mysql');
+
+        var connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'adminroot',
+            database: 'portal_noticias'
+        });
+
+        {/* 2 parâmetros do método query
+            sql - consulta em si, 
+            função de callback - é o que será feito após a consulta ser realizada */}
+        connection.query('select * from noticias', function (erro, result) {
+            res.render('noticias/noticias', { noticias: result });
+        });
+
     });
 }
