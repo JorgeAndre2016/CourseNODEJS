dc5d1b17fa117ef4509614859e7ff852e53190b0
Jorge <jorge_aquino@hotmail.com.br>
2019-02-01 21:38:48 -0200

    Configurando rotas com consign

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var app = require('./config/server');
-var rotaFormulario = require('./app/routes/formulario_inclusao_noticia')(app);
-var rotaHome = require('./app/routes/home')(app);
-var rotaNoticias = require('./app/routes/noticias')(app);
+// var rotaFormulario = require('./app/routes/formulario_inclusao_noticia')(app);
+// var rotaHome = require('./app/routes/home')(app);
+// var rotaNoticias = require('./app/routes/noticias')(app);
 
 app.listen(3000, function() {
     console.log("Servidor rodando com Express");
--- config/server.js
+++ config/server.js
@@ -1,8 +1,10 @@
 var express = require('express'); // import library express
+var consign = require('consign'); // import library consign
 
 var app = express(); // excutando função que modulo retorna
-
 app.set('view engine', 'ejs'); // engine de view do express agora é o EJS
 app.set('views', './app/views'); // definindo diretório das views
 
+consign().include('app/routes').into(app); // consign reconhece todos arquivos da pasta routes (faz um scan)
+                                      // pega estes módulos e inclui dentro do servidor
 module.exports = app;
