3ec462351a0f05d968629a197198860a67a3c6af
Jorge <jorge_aquino@hotmail.com.br>
2019-02-01 23:19:11 -0200

    Inserindo models

---
+++ app/models/noticiasModel.js
@@ -0,0 +1,15 @@
+module.exports = function () {
+
+    this.getNoticias = function(connection, callback){
+        {/* 2 parâmetros do método query
+            sql - consulta em si, 
+            função de callback - é o que será feito após a consulta ser realizada */}
+            connection.query('select * from noticias', callback);
+    }
+
+    this.getNoticia = function(connection, callback) {
+        connection.query('select * from noticias where id_noticia = 3', callback);
+    }
+
+    return this;
+}
