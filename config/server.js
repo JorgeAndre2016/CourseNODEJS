716f25c43a43cdf6b19f088ffa2347dc7cad6366
Jorge <jorge_aquino@hotmail.com.br>
2019-02-01 23:20:30 -0200

    início codificação formulário

--- config/server.js
+++ config/server.js
@@ -1,14 +1,17 @@
 var express = require('express'); // import library express
 var consign = require('consign'); // import library consign
+var bodyParser = require('body-parser');
 
 var app = express(); // excutando função que modulo retorna
 app.set('view engine', 'ejs'); // engine de view do express agora é o EJS
 app.set('views', './app/views'); // definindo diretório das views
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 consign()
     .include('app/routes') // consign reconhece todos arquivos da pasta routes (faz um scan)
     .then('config/dbConnections.js')
+    .then('app/models')
     .into(app);            // pega estes módulos e inclui dentro do servidor
-
 
 module.exports = app;
