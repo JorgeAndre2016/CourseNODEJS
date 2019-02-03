a42f4f5ed5c550575bf2079d2331239b1fc41831
Jorge <jorge_aquino@hotmail.com.br>
2019-02-03 19:51:27 -0200

    refactor in code NoticiasDAO

---
+++ app/models/NoticiasDAO.js
@@ -0,0 +1,22 @@
+function NoticiasDAO(connection) {
+    this._connection = connection;
+}
+
+NoticiasDAO.prototype.getNoticias = function(callback){
+    {/* 2 parâmetros do método query
+        sql - consulta em si, 
+        função de callback - é o que será feito após a consulta ser realizada */}
+        this._connection.query('select * from noticias', callback);
+}
+
+NoticiasDAO.prototype.getNoticia = function(callback) {
+    this._connection.query('select * from noticias where id_noticia = 3', callback);
+}
+
+NoticiasDAO.prototype.salvarNoticias = function(noticia, callback) {
+    this._connection.query('insert into noticias set ?', noticia, callback);
+}
+
+module.exports = function () {
+    return NoticiasDAO;
+}
