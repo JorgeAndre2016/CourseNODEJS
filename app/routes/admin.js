e6ef69c81b8eadea7e3b3cc432f5daa2e9e061c8
Jorge <jorge_aquino@hotmail.com.br>
2019-02-03 20:41:37 -0200

    insert validation in fields

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -6,6 +6,21 @@ module.exports = function (application) {
     application.post('/noticias/salvar', function (req, res) {
         var noticia = req.body; // express só preenche se estiver inserido o módulo body-parser
         
+        req.assert('titulo', 'Título é obrigatório').notEmpty();
+        req.assert('resumo', 'Resumo é obrigatório').notEmpty();
+        req.assert('resumo', 'Resumo deve conter entre 10 e 100 caracteres').len(10, 100);
+        req.assert('autor', 'Autor é obrigatório').notEmpty();
+        req.assert('data_noticia', 'Data é obrigatório').notEmpty();
+        // req.assert('data_noticia', 'Data é obrigatório').notEmpty().isDate({format: 'YYYY-mm-dd'});
+        req.assert('noticia', 'Notícia é obrigatório').notEmpty();
+
+        var erros = req.validationErrors();
+
+        if(erros){
+            res.render('admin/form_add_noticia')
+            return;
+        }
+
         var connection = application.config.dbConnections();
         var noticiaDao = new application.app.models.NoticiasDAO(connection); // acessando modulo
 
