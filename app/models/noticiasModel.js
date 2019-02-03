a9b80d0d213af60166d0df9729dbb12bf3a743e8
Jorge <jorge_aquino@hotmail.com.br>
2019-02-03 19:32:16 -0200

    Insert method salvarNoticias

--- app/models/noticiasModel.js
+++ app/models/noticiasModel.js
@@ -11,5 +11,9 @@ module.exports = function () {
         connection.query('select * from noticias where id_noticia = 3', callback);
     }
 
+    this.salvarNoticias = function(noticia, connection, callback) {
+        connection.query('insert into noticias set ?', noticia, callback);
+    }
+
     return this;
 }
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,10 +1,18 @@
-module.exports = function (app) {
-    app.get('/formulario_inclusao_noticia', function (req, res) {
+module.exports = function (application) {
+    application.get('/formulario_inclusao_noticia', function (req, res) {
         res.render('admin/form_add_noticia')
     });
 
-    app.post('/noticias/salvar', function (req, res) {
-        var noticias = req.body; // express só preenche se estiver inserido o módulo body-parser
-        res.render('Chegou na página')
+    application.post('/noticias/salvar', function (req, res) {
+        var noticia = req.body; // express só preenche se estiver inserido o módulo body-parser
+        
+        var connection = application.config.dbConnections();
+        var noticiasModel = application.app.models.noticiasModel; // acessando modulo
+
+        noticiasModel.salvarNoticias(noticia, connection, function (erro, result) {
+            res.redirect('/noticias')
+        });
+
+        connection.end(function(err) { console.log('Conexão com bd foi encerrada'); })
     });
 }
