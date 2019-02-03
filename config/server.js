a74943458e8925399a054849a5b03aec837628a4
Jorge <jorge_aquino@hotmail.com.br>
2019-02-03 19:57:38 -0200

    insert express-validator in project

--- config/server.js
+++ config/server.js
@@ -1,13 +1,14 @@
 var express = require('express'); // import library express
 var consign = require('consign'); // import library consign
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
 var app = express(); // excutando função que modulo retorna
 app.set('view engine', 'ejs'); // engine de view do express agora é o EJS
 app.set('views', './app/views'); // definindo diretório das views
 
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(expressValidator()); // inserindo middleware na aplicação
 consign()
     .include('app/routes') // consign reconhece todos arquivos da pasta routes (faz um scan)
     .then('config/dbConnections.js')
